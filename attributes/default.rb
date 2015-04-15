b5c60f5f05bedffcefd4574c5b66cffdd8aed0e1
Hao Xia <hao@optimizely.com>
2015-04-15 11:59:28 -0700

    add config to enable cache

--- attributes/default.rb
+++ attributes/default.rb
@@ -41,6 +41,8 @@ default[:druid][:broker][:properties]["druid.port"] = 8080
 default[:druid][:broker][:properties]["druid.monitoring.monitors"] = common_monitors + ["io.druid.client.cache.CacheMonitor"]
 default[:druid][:broker][:properties]["druid.cache.type"] = 'local'
 default[:druid][:broker][:properties]["druid.cache.sizeInBytes"] = 2**20 * 100
+default[:druid][:broker][:properties]["druid.broker.cache.useCache"] = "true"
+default[:druid][:broker][:properties]["druid.broker.cache.populateCache"] = "true"
 
 # Coordinator specific config
 default[:druid][:coordinator][:properties]["druid.service"] = "coordinator"
@@ -57,6 +59,8 @@ default[:druid][:historical][:properties]["druid.port"] = 8083
 default[:druid][:historical][:properties]["druid.monitoring.monitors"] = common_monitors + ["io.druid.client.cache.CacheMonitor","io.druid.server.metrics.ServerMonitor"]
 default[:druid][:historical][:properties]["druid.cache.type"] = 'local'
 default[:druid][:historical][:properties]["druid.cache.sizeInBytes"] = 2**20 * 100
+default[:druid][:historical][:properties]["druid.historical.cache.useCache"] = "true"
+default[:druid][:historical][:properties]["druid.historical.cache.populateCache"] = "true"
 
 # Overlord specific config
 default[:druid][:overlord][:properties]["druid.service"] = "overlord"
